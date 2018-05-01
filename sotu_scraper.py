9a27a842973920f40338c65bfbe8a1d1ff55bb22
Ratul Esrar <ratule@gmail.com>
2018-05-01 16:39:06 -0500

    updated scraper

--- sotu_scraper.py
+++ sotu_scraper.py
@@ -5,12 +5,11 @@
 
 import bs4 
 import requests
-import csv
 import pandas as pd
-import re
-import os
-from urllib.request import urlretrieve
-from urllib.parse import urlparse
+import sys
+
+
+BASE_URL = 'http://www.presidency.ucsb.edu/sou.php'
 
 
 def get_soup(url):
@@ -39,3 +38,32 @@ def get_transcript_links(url):
 					url_dict['url'].append(a['href'])
 
 	return pd.DataFrame(url_dict).sort_values(by='year', ascending=False)
+
+
+def scrape_sotu_text(df):
+	'''
+	Take a dataframe of urls and grabs the speech text from that url and its author
+	'''
+	speech_dict = {
+		'president':[],
+		'date':[],
+		'speech':[]
+	}
+	for url in df.url:
+		soup = get_soup(url)
+		pres_name = str(soup.find('meta', {'name':'title'})).split(':')[0].lstrip('<meta content="')
+		address_day = str(soup.find('meta', {'name':'title'})).split(':')[1].split(' - ')[1].strip('" name="title"/>')
+		content = str(soup.get_text()).split('\nCitation')[0].split('ProjectPromote Your Page Too\n\n\n')[1]
+
+		speech_dict['president'].append(pres_name)
+		speech_dict['date'].append(address_day)
+		speech_dict['speech'].append(content)
+
+	return pd.DataFrame(speech_dict).sort_values(by='date', ascending=False)
+
+
+if __name__ == '__main__':
+	out_file = open(sys.argv[-1], 'w') if len(sys.argv) is 1 else 'sotu_content.pkl'
+	urls_df = get_transcript_links(BASE_URL)
+	content_df = scrape_sotu_text(urls_df)
+	content_df.to_pickle(out_file)
